810302977ae866db132708ef9fccf8af432059d7
hugohvm25 <hgmello25@gmail.com>
2024-05-25 19:55:13 -0300

    Conexão com a tabela do banco de dados para exibição no site

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ import (
 )
 
 func conectaComBancoDeDados() *sql.DB {
-	// Corrigindo a string de conexão
-	conexao := "user=postegres dbname=loja_hugo password=1234 host=localhost sslmode=disable"
+	// criando a conexao com o banco de dados
+	conexao := "user=postgres dbname=loja_hugo password=123456 host=localhost sslmode=disable"
 	db, err := sql.Open("postgres", conexao)
 	if err != nil {
 		panic(err.Error())
@@ -32,6 +32,7 @@ func conectaComBancoDeDados() *sql.DB {
 
 // criando uma estrutura para que sejam armazenados novos produtos de forma automatizada sem ter que ficar criando tabelas dentro do arquivo HTML individualmente
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -42,8 +43,10 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	db := conectaComBancoDeDados()
-	defer db.Close()
+
+	//teste de conexão
+	// db := conectaComBancoDeDados()
+	// defer db.Close()
 	//sempre que tiver um / ela vai respoder para um segundo parametro - NÃO ESQUECER DO : PARA DETERMINAR A ROTA HTML
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
@@ -51,14 +54,47 @@ func main() {
 
 // sempre que tiver uma requisição no site (index) vou poder escrever e mostrar
 func index(w http.ResponseWriter, r *http.Request) {
-	//criando um Slice (array em go)
-	produtos := []Produto{
-		//2 formas diferentes de armazenar os dados no slice pela struct: passando os tipos e somente os dados diretamente
-		{Nome: "Camisa", Descricao: "Azul", Preco: 25, Quantidade: 4},
-		{"Bermuda", "Verde", 80, 2},
-		{"Tenis", "Preto", 100, 1},
-		{"Teste Produto", "Diferente", 1000, 1},
-		{"Teste Produto2", "Diferente", 1000, 1},
+	db := conectaComBancoDeDados()
+	selecProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//instanciando 1 produto depois armazenar no slice para exibir na página com um laço de repetição FOR
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selecProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		//vai scanear cada linha e armazenar as informações de cada produto no endereço &
+		err = selecProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		//após o scaneamento vai inserir na variavel p cada dado para ser incluido no slice
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		//adicionar a listas de produtos um a um
+		produtos = append(produtos, p)
 	}
 	temp.ExecuteTemplate(w, "Index", produtos)
+
+	//obrigatoriamente fecha o banco de dados
+	defer db.Close()
+
+	//criando um Slice (array em go)
+	// produtos := []Produto{
+	// 	//2 formas diferentes de armazenar os dados no slice pela struct: passando os tipos e somente os dados diretamente
+	// 	{Nome: "Camisa", Descricao: "Azul", Preco: 25, Quantidade: 4},
+	// 	{"Bermuda", "Verde", 80, 2},
+	// 	{"Tenis", "Preto", 100, 1},
+	// 	{"Teste Produto", "Diferente", 1000, 1},
+	// 	{"Teste Produto2", "Diferente", 1000, 1},
 }
